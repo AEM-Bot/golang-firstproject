7a93de7609ea90d08778cfccb9e1655980ad48be
Sasanka Reddy Chintam <schintam@victoria.com>
2023-03-30 17:33:31 -0400

    update unit tests

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
+	//check total length is 16
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got %d", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	//check first card is Ace of Spades
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	//check last card is Four of Clubs
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
